41b80935c2da0c36bca6e4f3f9500d9992e48e42
hallieraew <hallieweintraub@gmail.com>
2020-01-06 23:36:24 -0600

    variables and some function work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,42 @@
 // function for displaying image
 // function for clicked answer
 // function for displaying correct - or is this an if statement inside of the question function?
+// is this a function or an onject?
+// how do we update the DOM? ONLY use jquery for updating html and not for the javascript - this is javascript interval timing
+// 
+var correct = 0;
+var incorrect = 0;
+var noAnswer = 0;
+var answerSelect = false;
+var time = 30;
+var interval;
+
+var questionSet = [{
+    question: "Which season was the only one without a Thanksgiving episode?",
+    answers: [1, 3, 2, 6],
+    correct: 2,
+    image: ("../images/thanksgiving.webp")
+},
+{
+    question: "What is the name of Joey's stuffed penguin?",
+    answers: ["Hugsy", "Buddy", "Paolo", "Emily"],
+    correct: "Hugsy",
+    image: ("../images/hugsy-3.jpg")
+}
+];
+console.log(questionSet[1].correct)
+
+function startTimer() {
+    clearInterval(interval);
+    interval = setInterval(decrement, 1000);
+  }
+
+  function decrement() {
+
+    time--;
+
+    $(".time").push(time);
+  };
+
+  startTimer();
+  console.log(time);
