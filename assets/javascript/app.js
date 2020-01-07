8efa96c57498b49f29c16f25dcd7eebbd8e55f41
hallieraew <hallieweintraub@gmail.com>
2020-01-06 18:01:57 -0600

    some pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// set variable to hold interval time
+// count function per second
+// clock running true false set inside an if statement so only one interval is running at once 
+// reset function
+// question set - question, answer, image, correct answer -- one variable 
+// 
+// function for displaying image
+// function for clicked answer
+// function for displaying correct - or is this an if statement inside of the question function?
